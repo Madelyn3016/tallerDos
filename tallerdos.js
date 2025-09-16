bb475f46dd19fd7a2827f39058d8f42cad914760
Madelyn <madelyn2347@gmail.com>
2025-09-16 12:53:11 -0500

    parte 2: diferencia var, let y const

---
+++ tallerdos.js
@@ -0,0 +1,36 @@
+// Parte 1: Variables
+
+let nombre = "Madelyn";
+
+let edad = 18;
+
+let gustoProgramacion = true;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Me gusta la programacion:", gustoProgramacion);
+
+// Parte 2: Diferencia entre var, let y const
+
+let ciudad = "Neiva";
+
+const pais = "Canada";
+
+var color = "Morado";
+
+console.log("Ciudad:", ciudad);
+console.log("Pais:", pais);
+console.log("Color:", color);
+
+// Cambio en let
+ciudad = "Cali";
+console.log("Ciudad:", ciudad);
+
+//Cambio en const
+pais = "Colombia";
+
+//Cambio var
+color = {negro};
+
+//Diferencia con let: con let permite cambiar el valor de la variable, con const no se puede ya que es una constante, el valor permanece igual
+
